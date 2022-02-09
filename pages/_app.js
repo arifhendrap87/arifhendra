387d843e5c2d5e2e138db2fe5e258d3b454545bb
Febri <1102.adiputra@gmail.com>
2022-02-09 12:55:10 +0700

    update

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,20 @@
 import '../styles/globals.css'
 
+
+
+import '../public/fastland-html/css/bootstrap.css'
+import '../public/fastland-html/fonts/icon-font/css/style.css'
+// import '../public/fastland-html/fonts/typography-font/typo.css'
+// import '../public/fastland-html/fonts/fontawesome-5/css/all.css'
+
+import '../public/fastland-html/plugins/aos/aos.min.css'
+import '../public/fastland-html/plugins/fancybox/jquery.fancybox.min.css'
+import '../public/fastland-html/plugins/nice-select/nice-select.min.css'
+import '../public/fastland-html/plugins/slick/slick.min.css'
+
+import '../public/fastland-html/plugins/theme-mode-switcher/switcher-panel.css'
+import '../public/fastland-html/css/main.css'
+
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
 }
